f0311c0963dd5358b676b49d430475e25766af47
jedig <jakehairford@gmail.com>
2025-04-19 17:45:13 -0500

    score done

--- src/project.py
+++ src/project.py
@@ -102,7 +102,7 @@ class Block:
         if self.rotation_state == 0:
             self.rotation_state = len(self.cells) - 1
 
-    def draw(self, screen):
+    def draw(self, screen, offset_x, offset_y):
         tiles = self.get_cell_positions()
         for tile in tiles:
             tile_rect = pygame.Rect(tile.column*self.cell_size +11, tile.row*self.cell_size +11, 
@@ -213,6 +213,7 @@ class Game:
         self.current_block = self.get_random_block()
         self.next_block = self.get_random_block()
         self.game_over = False
+        self.score = 0
 
     def update_score(self, lines_cleared, move_down_points):
         if lines_cleared == 1:
@@ -330,12 +331,15 @@ def main():
                     game.move_right()
                 if event.key == pygame.K_DOWN and game.game_over == False:
                     game.move_down()
+                    game.update_score(0,1)
                 if event.key == pygame.K_UP and game.game_over == False:
                     game.rotate()
             if event.type == GAME_UPDATE and game.game_over == False:
                 game.move_down()
 
         #drawing
+        score_value_surface = title_font.render(str(game.score), True, Colors.white)
+
         screen.fill(Colors.dark_blue)
         screen.blit(score_surface, (365,20,50,50))
         screen.blit(next_surface, (375,180,50,50))
@@ -344,6 +348,8 @@ def main():
             screen.blit(game_over_surface, (320,450,50,50))
 
         pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+        screen.blit(score_value_surface, score_value_surface.get_rect(centerx= score_rect.centerx,
+                    centery = score_rect.centery))
         pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
         game_grid.draw(screen)
         game.draw(screen)
