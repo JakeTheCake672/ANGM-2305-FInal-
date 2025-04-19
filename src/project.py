61d40bcab1a621b9a21bc566e49776302754c4fe
jedig <jakehairford@gmail.com>
2025-04-19 13:50:37 -0500

    added grid.py for the grid system

--- src/project.py
+++ src/project.py
@@ -5,6 +5,8 @@ import random
 
 def main():
     pygame.init()
+    #colors
+    dark_blue = (44,44,127)
     screen = pygame.display.set_mode((300,600))
 
     pygame.display.set_caption("Pytris")
@@ -16,8 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
+        #drawing
+        screen.fill(dark_blue)
         pygame.display.update()
-
+        clock.tick(60)
 if __name__ == "__main__":
     main()
