c43e1fba929d27832977957df9fd3dc574c2ca27
jedig <jakehairford@gmail.com>
2025-04-19 13:38:19 -0500

    changed the video Im going off of since I got in confused with another one. Whoops. Will probably add more features instead since this one is more detailed

--- src/project.py
+++ src/project.py
@@ -1,2 +1,15 @@
 import pygame
 import random
+#tutorial im following
+#https://www.youtube.com/watch?v=nF_crEtmpBo&t=1s
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((300,600))
+    
+    pygame.display.set_caption("Pytris")
+
+    clock = pygame.time.Clock()
+
+if __name__ == "__main__":
+    main()
