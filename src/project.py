eea7fd031cb88b851662396330d4efbc2c5d2eb7
jedig <jakehairford@gmail.com>
2025-04-19 13:52:02 -0500

    moved the grid.py script into project.py.

--- src/project.py
+++ src/project.py
@@ -2,7 +2,17 @@ import pygame, sys
 import random
 #tutorial im following
 #https://www.youtube.com/watch?v=nF_crEtmpBo&t=1s
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_cols = 10
+        self.cell_size = 30
+        self.grid = [[0 for j in range(self.num_cols) for i in range(self.num_rows)]]
 
+    def print_grid(self):
+        for row in range()
+
+        
 def main():
     pygame.init()
     #colors
