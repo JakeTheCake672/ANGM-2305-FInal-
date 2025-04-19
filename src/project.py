2b65ef114c5163b624ca46ec3fc2576f149bea55
jedig <jakehairford@gmail.com>
2025-04-19 17:27:13 -0500

    added game over screen and more UI

--- src/project.py
+++ src/project.py
@@ -283,8 +283,11 @@ def main():
     
     title_font = pygame.font.Font(None, 40)
     score_surface = title_font.render("Score", True, Colors.white)
+    next_surface = title_font.render("Next", True, Colors.white)
+    game_over_surface = title_font.render("Game Over", True, Colors.white)
 
     score_rect = pygame.Rect(320,55,170,60)
+    next_rect = pygame.Rect(320,215,170,180)
     
     screen = pygame.display.set_mode((500,620))
 
@@ -323,7 +326,13 @@ def main():
         #drawing
         screen.fill(Colors.dark_blue)
         screen.blit(score_surface, (365,20,50,50))
+        screen.blit(next_surface, (375,180,50,50))
+
+        if game.game_over == True:
+            screen.blit(game_over_surface, (320,450,50,50))
+
         pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+        pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
         game_grid.draw(screen)
         game.draw(screen)
         
